36c3afd98585523a0bf32ddfc9bfdfc55ab9041c
Mehul Pathak <pathakkmehul@gmail.com>
2023-12-24 12:47:41 +0530

    separated header

---
+++ components/header.tsx
@@ -0,0 +1,37 @@
+// components/Header.tsx
+import React from "react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+const Header = () => {
+  return (
+    <header className="flex items-center justify-between mb-10">
+      <div className="flex items-center gap-2 text-lg font-semibold sm:text-base custom-text-color">
+        <img src="/favicon.png" alt="Logo" className="w-8 h-8" />
+        <Link className="" href="/">GPA Calculator</Link>
+      </div>
+      <div className="flex items-center gap-10">
+        <Link className="underline howToUse" href="/howtouse">
+          How to Use
+        </Link>
+        <Link href="https://github.com/m3hu1/gpa-calculator">
+          <Button className="rounded-full" size="icon" variant="ghost">
+            <img
+              alt="GitHub Icon"
+              height="32"
+              src="/github.png"
+              style={{
+                aspectRatio: "32/32",
+                objectFit: "cover",
+              }}
+              width="32"
+            />
+            <span className="sr-only">GitHub Profile</span>
+          </Button>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
