a4c87c0c67fbe46bfef3367a2c44194e655ae9d7
m3hu1 <pathakkmehul@gmail.com>
2024-05-06 18:16:56 +0530

    updated footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,15 +7,46 @@ const Footer = () => {
     <footer className="text-center py-4">
       <Typist>
         <p>
-          {"Developed by "}
-          <Link
+          Developed by{" "}
+          {/* <Link
             href="https://github.com/m3hu1"
             className="text-green-500 hover:underline"
             target="_blank"
             rel="noopener noreferrer"
+          > */}
+            <span className="underline">Mehul</span>
+          {/* </Link> */}
+        </p>
+        <p>
+          {" "}
+          <Link
+            href="https://www.github.com/m3hu1"
+            className="text-green-500 hover:underline"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <Typist.Delay ms={500} />
-            Mehul
+            <img
+              src="/git.jpeg"
+              alt="Instagram Logo"
+              className="inline h-5 w-5 mr-1"
+            />
+            GitHub
+          </Link>
+        </p>
+        <p>
+          {" "}
+          <Link
+            href="https://www.instagram.com/apparently.mehul"
+            className="text-pink-500 hover:underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="/i.jpeg"
+              alt="Instagram Logo"
+              className="inline h-5 w-5 mr-1"
+            />
+            Instagram
           </Link>
         </p>
       </Typist>
